ec4e6bf7e78395c9ab64db6f3e100d6320fccd1b
CochiorHeghiLucian <lucian.cochiorheghi@gmail.com>
2019-11-26 21:53:01 +0200

    Read data from site

--- src/main/java/com/example/demo/dataread/DataReadNumbers.java
+++ src/main/java/com/example/demo/dataread/DataReadNumbers.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
+import java.util.Set;
+
 public class DataReadNumbers {
     private static final String SUPERBER_FIST_PAGE = "https://www.superbet.ro";
     private static WebDriver webDriver;
@@ -15,33 +18,60 @@ public class DataReadNumbers {
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
 
+
         webDriver.navigate().to(SUPERBER_FIST_PAGE);
 
         Thread.sleep(2000);
         webDriver.findElement(By.xpath("//div[@class='header__right']/button[1]")).click();
 
         WebElement login = webDriver.findElement(By.xpath("//input[@class='input__field']"));
-        Thread.sleep(500);
+        Thread.sleep(1000);
         WebElement password = webDriver.findElement(By.xpath("//input[@type='password']"));
-        login.sendKeys("");
-        password.sendKeys("");
-        Thread.sleep(500);
+        webDriver.findElement(By.xpath("//*[@id='CybotCookiebotDialogBodyLevelButtonAccept']")).click();
+        login.sendKeys();
+        password.sendKeys();
+        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//button[@class='btn btn__primary btn--block btn']")).click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//i[@class='icon icon-menu']")).click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         webDriver.findElement(By.xpath("//a[@href='/casino-live']")).click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
 
         //hover
         Actions action = new Actions(webDriver);
-        WebElement we = webDriver.findElement(By.xpath("(//div[@class='casino-games__item-betting-range-wrapper'])[3]"));
+        WebElement we = webDriver.findElement(By.xpath("//*[@id=\"app\"]/div[3]/div/div/div/div/div/div[3]/div/div[3]/div/div"));
+        String parentWindow = webDriver.getWindowHandle();
         Thread.sleep(1000);
-        action.moveToElement(we).moveToElement(webDriver.findElement(By.xpath("(//button[@class='btn btn--md btn--w-144 btn__primary--yellow casino-games__item-play'])[3]"))).click().build().perform();
+        action.moveToElement(we).moveToElement(webDriver.findElement(By.xpath("//*[@id=\"app\"]/div[3]/div/div/div/div/div/div[3]/div/div[3]/div/button"))).click().build().perform();
 
 //        webDriver.findElement(By.xpath("(//button[@class='btn btn--md btn--w-144 btn__primary--yellow casino-games__item-play'])[3]")).click();
-        Thread.sleep(1000000);
+        Thread.sleep(1000);
 
+        Set<String> handles = webDriver.getWindowHandles();
+        for (String windowHandle : handles) {
+            if (!windowHandle.equals(parentWindow)) {
+                webDriver.switchTo().window(windowHandle);
+                webDriver.manage().window().maximize();
+                System.out.println(webDriver.getTitle());
+            }
+        }
+
+        Thread.sleep(5000);
+
+        webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[12]/div/div/div/div[2]/div/div[1]/button")).click();
+
+        Thread.sleep(2000);
+
+        webDriver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div[3]/div[2]/div/div[1]/div/div[2]/div[3]/img")).click();
+
+        List<WebElement> numbers = webDriver.findElements(By.cssSelector("div[class='number-container--1mKjk statistics--3-rTs']>div>span"));
+
+        for (WebElement element:numbers){
+            System.out.println(element.getText());
+        }
+
+        Thread.sleep(10000);
 
         if (webDriver != null) {
             webDriver.quit();
